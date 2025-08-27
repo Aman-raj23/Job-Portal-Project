44f53a0a77d19aa3a3528f4f7a2be89847dd7950
Aman Raj <23raj.aman03@gmail.com>
2025-08-27 23:56:02 +0530

    Update company.php

--- Jb_system/company.php
+++ Jb_system/company.php
@@ -479,7 +479,7 @@ $page = 1;
 									
 										<div class="footer-about-us">
 											<h5 class="footer-title">About Nightingale Jobs</h5>
-											<p>Nightingale Jobs is a job portal, online job management system developed by Aman Raj and team for his project in february 2018.</p>
+											<p>Nightingale Jobs is a job portal, online job management system developed by Aman Raj and team for his project in June 2024</p>
 										
 										</div>
 
@@ -507,7 +507,7 @@ $page = 1;
 							
 								<h5 class="footer-title">Nightingale Jobs Contact</h5>
 								
-								<p>Address :Bashirpura, Jalandhar, Punjab, 144001</p>
+								<p>Address :xyz, xyz, 144001</p>
 								<p>Email : <a href="mailto:03aman.raj2003@gmail.com">03aman.raj2003@gmail.com</a></p>
 								<p>Phone : <a href="tel:+233546607474">+233 546 607 474</a></p>
 								
