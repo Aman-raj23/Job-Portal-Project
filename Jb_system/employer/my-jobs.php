d18b788c4f1ebc8443f24fbb103f8dd4e3fe4489
8Aman Raj <23raj.aman03@gmail.com>
2025-09-05 23:51:13 +0530

    Update my-jobs.php

--- Jb_system/employer/my-jobs.php
+++ Jb_system/employer/my-jobs.php
@@ -392,7 +392,7 @@ $page = 1;
 									
 										<div class="footer-about-us">
 											<h5 class="footer-title">About Nightingale Jobs</h5>
-											<p>Nightingale Jobs is a job portal, online job management system developed by Nathaniel Nkrumah for his project in february 2018.</p>
+											<p>Nightingale Jobs is a job portal, online job management system developed by Aman Raj and team for his project in June 2024.</p>
 										
 										</div>
 
@@ -418,10 +418,10 @@ $page = 1;
 							
 							<div class="col-sm-12 col-md-3 mt-30-sm">
 							
-							<h5 class="footer-title">Nightingale Jobs Contact</h5>
+								<h5 class="footer-title">Nightingale Jobs Contact</h5>
 								
-								<p>Address : Takoradi, School Junction PO.BOX AX40</p>
-								<p>Email : <a href="mailto:nightingale.nath2@gmail.com">nightingale.nath2@gmail.com</a></p>
+								<p>Address : xyz, xyz, 144001</p>
+								<p>Email : <a href="mailto:03aman.raj2003@gmail.com">03aman.raj2003@gmail.com</a></p>
 								<p>Phone : <a href="tel:+233546607474">+233 546 607 474</a></p>
 								
 
@@ -442,7 +442,6 @@ $page = 1;
 						
 							<div class="col-sm-4 col-md-4">
 					
-								
 								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Nightingale Vision Software</p>
 								
 							</div>
@@ -450,7 +449,7 @@ $page = 1;
 							<div class="col-sm-4 col-md-4">
 							
 								<ul class="bottom-footer-menu">
-									<li><a >Developed by Nathaniel Nkrumah</a></li>
+									<li><a >Developed by Aman Raj and team</a></li>
 								</ul>
 							
 							</div>
