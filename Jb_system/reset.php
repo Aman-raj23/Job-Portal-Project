562411d8d5cb6067c75e759e1f8ca7d751099cd8
8Aman Raj <23raj.aman03@gmail.com>
2025-09-07 23:37:25 +0530

    Update reset.php

--- Jb_system/reset.php
+++ Jb_system/reset.php
@@ -301,7 +301,7 @@ print '
 									
 										<div class="footer-about-us">
 											<h5 class="footer-title">About Nightingale Jobs</h5>
-											<p>Nightingale Jobs is a job portal, online job management system developed by Nathaniel Nkrumah for his project in february 2018.</p>
+											<p>Nightingale Jobs is a job portal, online job management system developed by Aman Raj and team for his project in June 2024</p>
 										
 										</div>
 
@@ -329,9 +329,9 @@ print '
 							
 								<h5 class="footer-title">Nightingale Jobs Contact</h5>
 								
-								<p>Address : Takoradi, School Junction PO.BOX AX40</p>
-								<p>Email : <a href="mailto:nightingale.nath2@gmail.com">nightingale.nath2@gmail.com</a></p>
-								<p>Phone : <a href="tel:+233546607474">+233 546 607 474</a></p>
+								<p>Address : xyz, xyz, 144001</p>
+								<p>Email : <a href="mailto:03aman.raj2003@gmail.com">03aman.raj2003@gmail.com</a></p>
+								<p>Phone : <a href="tel:+912233445566">+91 22334 45566</a></p>
 								
 
 							</div>
@@ -358,7 +358,7 @@ print '
 							<div class="col-sm-4 col-md-4">
 							
 								<ul class="bottom-footer-menu">
-									<li><a >Developed by Nathaniel Nkrumah</a></li>
+									<li><a >Developed by Aman Raj and team</a></li>
 								</ul>
 							
 							</div>
